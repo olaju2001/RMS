253819a08e7231caabf0f4618352f5de73db11e3
Ola Mustafa <olaju2001@gmail.com>
2024-11-19 12:28:20 +0100

    Add initial project structure

---
+++ src/main/java/com/kisters/ResourceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kisters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kisters/ResourceManagementSystemApplicationTests.java
@@ -0,0 +1,22 @@
+package com.kisters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+    "spring.main.allow-bean-definition-overriding=true",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=sa"
+})
+class ResourceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test verifies that the Spring context loads successfully
+    }
+}
